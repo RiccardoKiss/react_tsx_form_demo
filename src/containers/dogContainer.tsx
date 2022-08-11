290fd5870161fa8fb270b63720f72d67a4f616f4
Riccardo Kiss <36801806+RiccardoKiss@users.noreply.github.com>
2022-08-11 16:32:15 +0200

    Updated dog image position to responsive values

--- src/containers/dogContainer.tsx
+++ src/containers/dogContainer.tsx
@@ -5,8 +5,8 @@ const DogImage = styled.img`
   position: absolute;
   height: 730px;
   width: 364px;
-  left: 870px;
-  top: 129px;
+  left: 60.417vw; /* 870px/1440px = 0.604166 */
+  top: 8.996vh; /* 129px/1434px = 0.089958158 */
 `;
 
 const DogContainer = () => {
